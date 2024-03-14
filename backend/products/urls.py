72fe36627fa17aab69c531e997b8e762b5f3b5ef
Goose1216 <goose1216@yandex.ru>
2024-03-14 15:37:05 +0300

    добавил имена для url в product

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import ProductList, ProductDetail
 urlpatterns = [
-    path('', ProductList.as_view()),
-    path('<slug:slug>/', ProductDetail.as_view()),
+    path('', ProductList.as_view(), name='product_list'),
+    path('<slug:slug>/', ProductDetail.as_view(), name='product_detail'),
 ]
