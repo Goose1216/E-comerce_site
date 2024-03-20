95768f19b1ca764fa8a81aa2235bb64d84a99d0b
Goose1216 <goose1216@yandex.ru>
2024-03-20 15:37:29 +0300

    Фикс прошлого коммита

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -17,23 +17,10 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from dj_rest_auth.views import PasswordResetConfirmView
-from django.urls import path, include, re_path
-from allauth.account.views import confirm_email
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from django.urls import path, include
 
 
 urlpatterns = [
                   path('admin/', admin.site.urls, name='admin'),
-                  path('api/v1/', include('products.urls'), name='home'),
-                  path('api-auth/', include('rest_framework.urls'), name="api_auth"),
-                  path('api/v1/dj-rest-auth/', include('dj_rest_auth.urls')),
-                  path('api/v1/dj-rest-auth/registration/', include('dj_rest_auth.registration.urls'),
-                       name='rest_registration'),
-                  path('api/v1/dj-rest-auth/password/reset/confirm/<uidb64>/<token>/',
-                       PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-                  re_path(r'^api/v1/dj-rest-auth/account-confirm-email/(?P<key>[-:\w]+)/$', confirm_email,
-                          name='account_confirm_email'),
-                  path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-                  path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='docs'),
+                  path("api/", include("api.urls")),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
